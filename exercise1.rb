27470e01b8c6748ed0ddf39eadcb5eac942ceed8
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-29 15:27:41 -0400

    Create Task class with description and due_date variables, instantiate 3 tasks and test reader and writer methods

---
+++ exercise1.rb
@@ -0,0 +1,37 @@
+class Task
+
+  def initialize(description, due_date)
+    @description = description
+    @due_date = due_date
+  end
+
+  def description
+    @description
+  end
+
+  def due_date
+    @due_date
+  end
+
+  def description=(new_description)
+    @description = new_description
+  end
+
+  def due_date=(new_due_date)
+    @due_date = new_due_date
+  end
+
+end
+
+task1 = Task.new("buy groceries", "Oct. 1")
+puts task1.description
+puts task1.due_date
+
+task1.due_date = "Nov. 1"
+puts task1.due_date
+
+task2 = Task.new("eat lots of ramen", "Sept. 30")
+puts task2.description
+
+task3 = Task.new("watch Rick and Morty", "Oct. 15")
+puts "Make sure you #{ task3.description } on #{ task3.due_date }!"
